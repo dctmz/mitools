0eaf736338e1fa43592d51f9b100111776f46115
dctxf <dctmz@qq.com>
2022-09-09 16:20:30 +0800

    add reg

--- src/reg/index.ts
+++ src/reg/index.ts
@@ -13,12 +13,28 @@ export const EMAIL_REG =
 export const ID_CARD_REG =
   /^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
 /** 重复字符 */
-export const REPEAT_TIMES = /(\w)\1{3}/g;
+export const REPEAT_TIMES_REG = /(\w)\1{3}/g;
 /** 特殊字符 */
-export const SPECIAL_CHARACTER = /[$@$!%*#?&.]/;
+export const SPECIAL_CHARACTER_REG = /[$@$!%*#?&.]/;
 /** 纯字母 */
-export const ONLY_LETTER = /[A-Za-z]/;
+export const ONLY_LETTER_REG = /[A-Za-z]/;
 /** 纯数字 */
-export const ONLY_NUMBER = /\d/;
+export const ONLY_NUMBER_REG = /\d/;
 /** 空格 */
-export const CONTAIN_SPACE = /\s/g;
+export const CONTAIN_SPACE_REG = /\s/g;
+/** 中文字符 */
+export const CHINESE_CHARACTER_REG = /[\u4e00-\u9fa5]/gm;
+/** 双字节字符 */
+export const DOUBLE_BYTE_CHARACTER_REG = /[^\x00-\xff]/gim;
+/** 行首尾空格 */
+export const LEADING_TRAILING_SPACES_REG = /(^\s*)|(\s*$)/;
+/** url */
+export const URL_REG =
+  /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i;
+export const IE_REG = /msie (\d+\.\d+)/i;
+export const WEBKIT_REG = /webkit/i;
+export const CHROME_REG = /chrome\/(\d+\.\d+)/i;
+export const FIREFOX_REG = /firefox\/(\d+\.\d+)/i;
+export const OPERA_REG =
+  /opera(\/| )(\d+(\.\d+)?)(.+?(version\/(\d+(\.\d+)?)))?/i;
+export const SAFARI_REG = /(\d+\.\d)?(?:\.\d)?\s+safari\/?(\d+\.\d+)?/i;
