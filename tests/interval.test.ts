4a3b7359929ff51f903439af5f7e063cc364a234
dctxf <dctmz@qq.com>
2023-05-17 14:18:24 +0800

    bk

--- tests/interval.test.ts
+++ tests/interval.test.ts
@@ -1,8 +1,9 @@
 import { describe, it, vi } from "vitest";
-import { requestInterval } from "../src/Interval";
+import { requestInterval } from "../src/interval";
 
 describe('interval test', () => {
   it('interval test', async () => {
+    vi.useFakeTimers()
     let i = 0
     requestInterval(() => console.log(++i), 50)
     vi.runOnlyPendingTimers()
