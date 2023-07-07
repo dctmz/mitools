1d312b199bb5c8c6be7ee6c5f2451ea6519b9a53
dctxf <dctmz@qq.com>
2023-07-07 13:53:45 +0800

    fix: bugs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { includePath, isUrl, openUrl, samePath } from "./url";
-import { isWechatDev } from "./user-agent";
+import { includePath, isUrl, openUrl, samePath } from './url';
+import { isWechatDev } from './user-agent';
 
-export { arrAddition } from './arithmetic';
-export { enum2options } from "./enum";
-export { isIdCardNo } from "./idcard";
-export { fastOssProcessUrl, getOssProcessUrl, transformObject } from "./oss";
-export { telTo } from "./phone";
+export { arrAddition, toFixed } from './arithmetic';
+export { enum2options } from './enum';
+export { isIdCardNo } from './idcard';
+export { fastOssProcessUrl, getOssProcessUrl, transformObject } from './oss';
+export { telTo } from './phone';
 export {
   CONTAIN_SPACE,
   EMAIL_REG,
@@ -14,17 +14,10 @@ export {
   ONLY_LETTER,
   ONLY_NUMBER,
   REPEAT_TIMES,
-  SPECIAL_CHARACTER
-} from "./reg";
-export { POWER_PASSWORD } from "./rules";
-export { sleep } from "./sleep";
-export { getRandomString } from "./string";
-export { isAndroid, isApple, isWxBrowser } from "./user-agent";
-export {
-  isWechatDev,
-  openUrl,
-  includePath,
-  samePath,
-  isUrl,
-};
-
+  SPECIAL_CHARACTER,
+} from './reg';
+export { POWER_PASSWORD } from './rules';
+export { sleep } from './sleep';
+export { getRandomString } from './string';
+export { isAndroid, isApple, isWxBrowser } from './user-agent';
+export { includePath, isUrl, isWechatDev, openUrl, samePath };
