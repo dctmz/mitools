51c38ed011d87f8b05d6fc19526848f232090e96
dctxf <dctmz@qq.com>
2023-07-07 13:45:40 +0800

    fix: bugs

--- src/arithmetic.ts
+++ src/arithmetic.ts
@@ -1,4 +1,19 @@
+/** 数学方法 */
+/**
+ * 输入数字或者字符串返回数字，保留两位小数
+ * @param num
+ * @returns {number}
+ */
+export const toFixed = (num?: string | number): number => {
+  if (!num) return 0;
+  if (typeof num === 'string') {
+    num = Number(num);
+  }
+  return Number(num.toFixed(2));
+};
+
+// 加法
 export const arrAddition = (arr?: (string | number | undefined)[]) => {
   if (!arr) return;
-  return arr.map((i) => (i ? Number(i) : 0)).reduce((a, b) => a + b);
+  return toFixed(arr.map(i => (i ? Number(i) : 0)).reduce((a, b) => a + b));
 };
--- tests/arithmetic.test.ts
+++ tests/arithmetic.test.ts
@@ -5,13 +5,14 @@ const arrMap = {
   6: [1, 2, 3],
   10: [1, 2, 3, 4],
   15: [1, 2, 3, 4, 5],
-  2: ['', '2']
-}
+  2: ['', '2'],
+  0.3: [0.1, 0.2],
+};
 
 describe('arithmetic', () => {
   it('should add', () => {
     for (const [key, value] of Object.entries(arrMap)) {
-      const res = arrAddition(value)
+      const res = arrAddition(value);
       expect(res).toBe(Number(key));
     }
   });
